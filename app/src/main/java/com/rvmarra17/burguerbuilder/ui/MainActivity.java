6e8334c8bd89b423bd7ed00937b0f70a957b4465
Raul Varandela <raulvarandela@protonmail.com>
2020-10-20 13:05:31 +0200

    first commit

---
+++ app/src/main/java/com/rvmarra17/burguerbuilder/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rvmarra17.burguerbuilder.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rvmarra17.burguerbuilder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
